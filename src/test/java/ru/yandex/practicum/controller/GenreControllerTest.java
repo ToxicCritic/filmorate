99ba94e9c7997d4d64b33d2f2420bc85570a95d1
ToxicCritic <Em0zzIoN@yandex.ru>
2025-01-26 03:29:32 +0300

    Добавить тесты для новых контроллеров

---
+++ src/test/java/ru/yandex/practicum/controller/GenreControllerTest.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.model.Genre;
+import ru.yandex.practicum.storage.genre.GenreDbStorage;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({GenreDbStorage.class})
+public class GenreControllerTest {
+
+    @Autowired
+    private GenreDbStorage genreStorage;
+
+    @Test
+    public void testFindAllGenres() {
+        List<Genre> genres = genreStorage.findAll();
+
+        assertThat(genres).isNotEmpty();
+        assertThat(genres.get(0)).hasFieldOrPropertyWithValue("id", 1L)
+                .hasFieldOrPropertyWithValue("name", "Action");
+    }
+
+    @Test
+    public void testFindGenreById() {
+        Optional<Genre> genreOptional = genreStorage.findById(1L);
+
+        assertThat(genreOptional)
+                .isPresent()
+                .hasValueSatisfying(genre ->
+                        assertThat(genre).hasFieldOrPropertyWithValue("id", 1L)
+                                .hasFieldOrPropertyWithValue("name", "Action")
+                );
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/controller/MpaControllerTest.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.model.MpaRating;
+import ru.yandex.practicum.storage.mpa.MpaDbStorage;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({MpaDbStorage.class})
+public class MpaControllerTest {
+
+    @Autowired
+    private MpaDbStorage mpaStorage;
+
+    @Test
+    public void testFindAllMpaRatings() {
+        List<MpaRating> ratings = mpaStorage.findAll();
+
+        assertThat(ratings).isNotEmpty();
+        assertThat(ratings.get(0)).hasFieldOrPropertyWithValue("id", 1)
+                .hasFieldOrPropertyWithValue("name", "G")
+                .hasFieldOrPropertyWithValue("description", "General Audiences");
+    }
+
+    @Test
+    public void testFindMpaRatingById() {
+        Optional<MpaRating> mpaRatingOptional = mpaStorage.findById(1L);
+
+        assertThat(mpaRatingOptional)
+                .isPresent()
+                .hasValueSatisfying(mpaRating ->
+                        assertThat(mpaRating).hasFieldOrPropertyWithValue("id", 1)
+                                .hasFieldOrPropertyWithValue("name", "G")
+                                .hasFieldOrPropertyWithValue("description", "General Audiences")
+                );
+    }
+}
